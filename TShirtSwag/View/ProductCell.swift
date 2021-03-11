e9e0353578308c49bbbfd92ce4b54b37c26459df
janpatrix <janpatrix@gmail.com>
2021-03-11 19:10:27 +0100

    Transfer

---
+++ TShirtSwag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  TShirtSwag
+//
+//  Created by Patrick Groß on 04.03.21.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+	
+	@IBOutlet weak var imageView: UIImageView
+	@IBOutlet weak var productTitle: UILabel
+	@IBOutlet weak var productPrice: UILabel
+}
